035a8a27c20f2787995c0721787c4268b867a2e3
Gyanthakur <gps.96169@gmail.com>
2025-03-05 19:04:21 +0530

    feat: added app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="bg-blue-300">
+			<div className="flex ">
+				<img className="w-24 h-14" src="/git_practice.png" alt="" />
+				<span className="flex items-center justify-center font-bold text-3xl">Git Practice</span>
+			</div>
+		</div>
+	);
+};
+
+export default App;
+App;
