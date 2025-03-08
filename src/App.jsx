75f50e945179fb06ca01812095447d49b2d3433a
Gyanthakur <gps.96169@gmail.com>
2025-03-09 02:23:47 +0530

    feat: update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+
+
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Terminal from "./components/Terminal";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Lesson from "./components/Lesson";
 
 const App = () => {
+
 	return (
-		<div className="bg-blue-300">
-			<div className="flex ">
-				<img className="w-24 h-14" src="/git_practice.png" alt="" />
-				<span className="flex items-center justify-center font-bold text-3xl">Git Practice</span>
+		<>
+			<Navbar />
+			<div className="flex h-screen">
+				{/* <Sidebar onSelectLesson={setSelectedLesson} /> */}
+				<div className="flex-1 flex flex-col">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/terminal" element={<Terminal />} />
+						<Route path="/lessons" element={<Lesson />} />
+					</Routes>
+				</div>
 			</div>
-		</div>
+			<Footer />
+		</>
 	);
 };
 
 export default App;
-App;
